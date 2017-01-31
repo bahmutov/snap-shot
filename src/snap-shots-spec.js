3e1fd071492c99e4bd1fad3d97b364c79da6212c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-01-30 23:36:48 -0500

    initial code

---
+++ src/snap-shots-spec.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/* global describe, it */
+describe('snap-shots', () => {
+  const snapShots = require('.')
+  it('write this test', () => {
+    console.assert(snapShots, 'should export something')
+  })
+})
