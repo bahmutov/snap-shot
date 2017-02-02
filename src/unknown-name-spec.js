f260c0d669a4d78d9dcfa58d115e440c5bddd778
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-02 12:37:55 -0500

    add a test to see how template literals behave for #14

--- src/unknown-name-spec.js
+++ src/unknown-name-spec.js
@@ -1,10 +1,16 @@
 const snapshot = require('.')
 
 /* global it */
-
 const testName = 'variable test name (value 30)'
 const value = 30
 it(testName, () => {
   // this is a test without hard coded name
+  snapshot(value)
+})
+
+const secondTest = 'second'
+it(`handles template literals ${secondTest}`, () => {
+  // this test has template literal instead of
+  // primitive string
   snapshot(value)
 })
