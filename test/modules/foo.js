e0120fdb32c5079d41bdff65b743dfe4d2f1d320
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-06 22:36:04 -0500

    testing stack-site

--- test/modules/foo.js
+++ test/modules/foo.js
@@ -1,5 +1,6 @@
 const callsites = require('callsites')
 const relativeTo = require('path').relative.bind(null, process.cwd())
+const stackSites = require('stack-sites')
 module.exports = function foo () {
   console.log('inside foo')
   // who called me?
@@ -8,4 +9,7 @@ module.exports = function foo () {
     relativeTo(caller.getFileName()),
     caller.getLineNumber(),
     caller.getColumnNumber())
+  const callerSite = stackSites()[2]
+  console.log('My caller is %s line %d column %d',
+    callerSite.filename, callerSite.line, callerSite.column)
 }
