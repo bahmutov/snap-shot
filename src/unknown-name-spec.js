4b91da5d04de9302a2e8e1eb2ba626a04ea30991
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-08 08:35:10 -0500

    chore(example): more unknown callback names

--- src/unknown-name-spec.js
+++ src/unknown-name-spec.js
@@ -14,9 +14,47 @@ it(`handles template literals ${secondTest}`, () => {
   // primitive string
   snapshot(value)
 })
+/* snapshot uses SHA of the callback function source
+exports['983ea... 1'] = "foo"
+*/
 
 const thirdTest = 'third test'
 it(thirdTest, function testThree () {
   snapshot('foo')
   snapshot('bar')
 })
+/* uses "testThree" name
+exports['testThree 1'] = "foo"
+
+exports['testThree 2'] = "bar"
+*/
+
+// create tests on the fly with single SHA
+const names = ['test A', 'test B', 'test C']
+names.forEach(name => {
+  it(name, () => {
+    snapshot(name + ' works')
+  })
+})
+/* snapshot will be something like
+exports['7464af... 1'] = "test A works"
+
+exports['7464af... 2'] = "test B works"
+
+exports['7464af... 3'] = "test C works"
+*/
+
+// create tests on the fly with single name
+const tests = ['test A', 'test B', 'test C']
+tests.forEach(name => {
+  it(name, function testSomething () {
+    snapshot(name + ' works')
+  })
+})
+/* snapshot will use "testSomething" name
+exports['testSomething 1'] = "test A works"
+
+exports['testSomething 2'] = "test B works"
+
+exports['testSomething 3'] = "test C works"
+*/
