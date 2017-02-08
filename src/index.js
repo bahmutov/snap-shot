b738b763aec5b3383900315ea8a87bab676032b2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-07 22:19:57 -0500

    chore(refactor): refactoring parsing source file

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@
 const debug = require('debug')('snap-shot')
 const debugSave = require('debug')('save')
 const stackSites = require('stack-sites')
-const falafel = require('falafel')
 const la = require('lazy-ass')
 const is = require('check-more-types')
-const crypto = require('crypto')
-const {snapshotIndex} = require('./utils')
+const utils = require('./utils')
+const {snapshotIndex} = utils
 
 const isNode = Boolean(require('fs').existsSync)
 const isBrowser = !isNode
@@ -27,93 +26,8 @@ const snapshotsPerTest = {}
 
 const shouldUpdate = Boolean(process.env.UPDATE)
 
-function isTestFunctionName (name) {
-  return ['it', 'test'].includes(name)
-}
-
-function isTestFunction (node) {
-  if (node.type === 'Identifier') {
-    return isTestFunctionName(node.name)
-  }
-  if (node.type === 'MemberExpression') {
-    return isTestFunctionName(node.object.name)
-  }
-}
-
-function sha256 (string) {
-  la(is.unemptyString(string), 'missing string to SHA', string)
-  const hash = crypto.createHash('sha256')
-  hash.update(string)
-  return hash.digest('hex')
-}
-
 function getSpecFunction ({file, line}) {
-  // TODO can be cached efficiently
-  la(is.unemptyString(file), 'missing file', file)
-  la(is.number(line), 'missing line number', line)
-  const source = fs.readFileSync(file, 'utf8')
-  let foundSpecName, specSource, startLine
-  const options = {
-    locations: true,
-    sourceType: 'module'
-  }
-
-  falafel(source, options, node => {
-    if (foundSpecName) {
-      // already found
-      return
-    }
-    if (node.type === 'CallExpression' &&
-      isTestFunction(node.callee) &&
-      node.loc.start.line < line &&
-      node.loc.end.line > line) {
-      debug('found test function around snapshot at line %d', line)
-
-      if (node.arguments.length !== 2) {
-        throw new Error('Cannot get test name for ' + node.source())
-      }
-
-      specSource = node.arguments[1].source()
-      startLine = node.loc.start.line
-
-      // TODO handle tests where just a single function argument was used
-      // it(function testThis() {...})
-      let specName
-
-      const nameNode = node.arguments[0]
-      if (nameNode.type === 'TemplateLiteral') {
-        specName = nameNode.source().replace(/`/g, '')
-        debug('template literal name "%s"', specName)
-      } else if (nameNode.type === 'Literal') {
-        specName = nameNode.value
-        debug('regular string name "%s", specName')
-      } else {
-        // TODO handle single function
-      }
-
-      if (node.arguments.length === 2 &&
-        node.arguments[1].type === 'FunctionExpression' &&
-        node.arguments[1].id &&
-        is.unemptyString(node.arguments[1].id.name)) {
-        specName = node.arguments[1].id.name
-        debug('callback function has a name "%s"', specName)
-      }
-
-      foundSpecName = specName
-
-      if (!foundSpecName) {
-        const hash = sha256(specSource)
-        debug('using source hash %s for found spec in %s line %d',
-          hash, file, line)
-        foundSpecName = hash
-      }
-    }
-  })
-  return {
-    specName: foundSpecName,
-    specSource,
-    startLine
-  }
+  return utils.getSpecFunction({file, line, fs})
 }
 
 const formKey = (specName, oneIndex) =>
--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,29 @@
 
 const la = require('lazy-ass')
 const is = require('check-more-types')
+const falafel = require('falafel')
+const crypto = require('crypto')
+const debug = require('debug')('snap-shot')
+
+function isTestFunctionName (name) {
+  return ['it', 'test'].includes(name)
+}
+
+function isTestFunction (node) {
+  if (node.type === 'Identifier') {
+    return isTestFunctionName(node.name)
+  }
+  if (node.type === 'MemberExpression') {
+    return isTestFunctionName(node.object.name)
+  }
+}
+
+function sha256 (string) {
+  la(is.unemptyString(string), 'missing string to SHA', string)
+  const hash = crypto.createHash('sha256')
+  hash.update(string)
+  return hash.digest('hex')
+}
 
 function snapshotIndex ({counters, specName}) {
   la(is.object(counters), 'expected counters', counters)
@@ -14,6 +37,76 @@ function snapshotIndex ({counters, specName}) {
   return counters[specName]
 }
 
+function getSpecFunction ({fs, file, line}) {
+  // TODO can be cached efficiently
+  la(is.unemptyString(file), 'missing file', file)
+  la(is.number(line), 'missing line number', line)
+  const source = fs.readFileSync(file, 'utf8')
+  let foundSpecName, specSource, startLine
+  const options = {
+    locations: true,
+    sourceType: 'module'
+  }
+
+  falafel(source, options, node => {
+    if (foundSpecName) {
+      // already found
+      return
+    }
+    if (node.type === 'CallExpression' &&
+      isTestFunction(node.callee) &&
+      node.loc.start.line < line &&
+      node.loc.end.line > line) {
+      debug('found test function around snapshot at line %d', line)
+
+      if (node.arguments.length !== 2) {
+        throw new Error('Cannot get test name for ' + node.source())
+      }
+
+      specSource = node.arguments[1].source()
+      startLine = node.loc.start.line
+
+      // TODO handle tests where just a single function argument was used
+      // it(function testThis() {...})
+      let specName
+
+      const nameNode = node.arguments[0]
+      if (nameNode.type === 'TemplateLiteral') {
+        specName = nameNode.source().replace(/`/g, '')
+        debug('template literal name "%s"', specName)
+      } else if (nameNode.type === 'Literal') {
+        specName = nameNode.value
+        debug('regular string name "%s", specName')
+      } else {
+        // TODO handle single function
+      }
+
+      if (node.arguments.length === 2 &&
+        node.arguments[1].type === 'FunctionExpression' &&
+        node.arguments[1].id &&
+        is.unemptyString(node.arguments[1].id.name)) {
+        specName = node.arguments[1].id.name
+        debug('callback function has a name "%s"', specName)
+      }
+
+      foundSpecName = specName
+
+      if (!foundSpecName) {
+        const hash = sha256(specSource)
+        debug('using source hash %s for found spec in %s line %d',
+          hash, file, line)
+        foundSpecName = hash
+      }
+    }
+  })
+  return {
+    specName: foundSpecName,
+    specSource,
+    startLine
+  }
+}
+
 module.exports = {
-  snapshotIndex
+  snapshotIndex,
+  getSpecFunction
 }
